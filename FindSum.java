bb3822988002f7355a9ad58d993eb59cbbe137f0
srinivast2018 <45238271+srinivast2018@users.noreply.github.com>
2018-11-21 21:50:52 +0530

    Add files via upload

---
+++ FindSum.java
@@ -0,0 +1,23 @@
+//Program to print the sum of two numbers without using + operator.
+package mypackage;
+
+public class FindSum {
+
+	//Declaration of main method
+	public static void main(String[] args) {
+
+		//Declaration of two integer numbers
+		int number1=10;
+		int number2=20;
+		//Calculating the sum of two numbers
+		int sum=number1-(-number2);
+		
+		System.out.println("First number is "+number1);
+		System.out.println("Second number is "+number2);
+		
+		//Displaying the sum of two numbers
+		System.out.println("Sum of two numbers: "+sum);
+		
+	}
+
+}//End of the program
